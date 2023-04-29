ce4a0fafd49c75e2730a664e1fd5ff0aec92fecf
Estelle Sterrett <estellesterrett@gmail.com>
2023-04-29 13:00:13 -0400

    multithreaded find_arb() method

--- src/router.py
+++ src/router.py
@@ -12,8 +12,13 @@ class Router:
        self.v = np.zeros(number_of_tokens)
     
     def find_arb(self, v):
-        # call find_arb on each cfmms
-        k = 0
+        def sub_method(i):
+            return self.cfmms[i].find_arb(v)
+        threads = []
+        with ThreadPoolExecutor as executor:
+            threads = list(executor.map(sub_method, range(len(self.cfmms))))
+        self.deltain, self.deltaout = zip(*threads)
+        return self.deltain, self.deltaout
     
     def route(self, v=None):
         # route trrade to each cfmms
